eac376486341702dcbc85c632c62ae6e00dcdafe
Yash Raj <yashrajlfs@gmail.com>
2025-06-23 22:02:52 +0530

    Include config.js in Git and deploy to Render

---
+++ server/config.js
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export default {
+  JWT_SECRET: process.env.JWT_SECRET,
+  EMAIL: process.env.EMAIL,
+  PASSWORD: process.env.PASSWORD,
+  ATLAS_URI: process.env.ATLAS_URI,
+};
